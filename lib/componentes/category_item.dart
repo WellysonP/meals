9b819e8aca4406a4748e142e2c99023ff7bac5b2
Wellyson Pontes <wellysonpontes@gmail.com>
2022-05-07 18:27:02 -0300

    Criado CATEGORIES_SCREEN.DART

---
+++ lib/componentes/category_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({Key? key}) : super(key: key);
+
+  final Category category;
+
+  const CategoryItem({
+    required this.category,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
