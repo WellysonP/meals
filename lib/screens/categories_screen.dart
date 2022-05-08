ef8acbf190aa771338da18cb6a2128b4b3c14f83
Wellyson Pontes <wellysonpontes@gmail.com>
2022-05-08 00:32:01 -0300

    treinando GIT COMMIT

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../componentes/category_item.dart';
+import '../data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      children: [],
+    );
+  }
+}
