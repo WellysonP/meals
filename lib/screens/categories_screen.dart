5d211ef8fa878e63da19798d018c169a3cf8da38
Wellyson Pontes <wellysonpontes@gmail.com>
2022-05-08 01:19:06 -0300

    Ajustado MAIND.DART para rodar arquivo CATEGORIES_SCREENS, ajustado Text e Colors em CATEGORY_ITEM

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -3,17 +3,20 @@ import '../componentes/category_item.dart';
 import '../data/dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
-  const CategoriesScreen({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20),
-      children: [],
+    return Scaffold(
+      appBar: AppBar(title: Text("Vamos cozinhar?")),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(category: cat);
+        }).toList(),
+      ),
     );
   }
 }
